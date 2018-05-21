148790c872abc0f3e9f40f429703419fa0310bec
Derek Garnett <drkgrntt@gmail.com>
2018-05-20 20:58:49 -0500

    add a header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './Header';
 import Landing from './Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <Header />
         <BrowserRouter>
           <div className="routes">
             <Route exact path="/" component={Landing} />
--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -4,9 +4,8 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <h1>Welcome to your new MERRN app!</h1>
-        <p>Redux and Redux-Form are already hooked up for you.</p>
-        <p>Don't forget to connect your server to MLab and Heroku!</p>
+        <h1>Remedy Home Furniture</h1>
+        <p>Custom home furniture in Kansas City.</p>
       </div>
     );
   }
